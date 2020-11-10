78eaf4bae3d8dd13e7de76d582929d20af81061e
tiedan <15122738156@163.com>
2020-11-10 23:55:37 +0800

    实现增删改查函数

--- practice/Day4/crud_express/student1.js
+++ practice/Day4/crud_express/student1.js
@@ -53,3 +53,58 @@ exports.findById = function(id, callback){
 		callback(null, student);
 	})
 }
+
+exports.updateById = function(student, callback){
+	fs.readFile(dbPath, function(err, data){
+		if(err){
+			callback(err);
+		}
+
+		let students = JSON.parse(data).students;
+		student.id = parseInt(student.id);
+
+		let stu = students.find(function(item){
+			return student.id === item.id;
+		})
+		console.log("学生:",student);
+
+		for(let key in student){
+			stu[key] = student[key];
+		}
+
+		let fileData = JSON.stringify({
+			students: students//这里为什么可以？？？？
+		})
+
+		fs.writeFile(dbPath, fileData, function(err){
+			if(err){
+				return callback(err);
+			}
+			callback(null);
+		})
+	})
+}
+
+exports.deleteById = function(id, callback){
+	fs.readFile(dbPath, 'utf8', function(err, data){
+		if(err){
+			return callback(err);
+		}
+		let students = JSON.parse(data).students;
+		let deleteId = students.findIndex(function(item){
+			return parseInt(id) === item.id
+		})
+
+		students.splice(deleteId, 1);
+		let fileData = JSON.stringify({
+			students: students
+		})
+
+		fs.writeFile(dbPath, fileData, function(err){
+			if (err) {
+				callback(err);
+			}
+			callback(null);
+		})
+	})
+}
